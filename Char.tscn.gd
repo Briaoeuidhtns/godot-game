d66660a5fcbcf8ced49ed54695571888fc6dab40
Brian Brunner <brian.brunner@mines.sdsmt.edu>
2019-07-30 19:40:54 -0500

    Restructure main and char
    wrap char so it has a place for inventory and scale
    
    rename constants

--- Char.tscn.gd
+++ Char.tscn.gd
@@ -1,41 +1,51 @@
 extends KinematicBody
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var speed = 5
-var gravity = 9.8
-var sensitivity = .1
+const GRAVITY = 9.8
+const SENSITIVITY = .05
 var falling = 0
+
+const DEACCEL = 3
+const MAX_SLOPE_ANGLE = deg2rad(40)
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func get_input():
 	var dir = Vector3(0,0,0)
-	if Input.is_action_pressed("action_move_forward"):
+	if Input.is_action_pressed("movement_forward"):
 		dir.z -= 1
-	if Input.is_action_pressed("action_move_backward"):
+	if Input.is_action_pressed("movement_backward"):
 		dir.z += 1
-	if Input.is_action_pressed("action_strafe_right"):
-		dir.x += 1
-	if Input.is_action_pressed("action_strafe_left"):
-		dir.x -= 1
-	return dir.normalized()
+	if Input.is_action_pressed("look"):
+		if Input.is_action_pressed("movement_left"):
+			dir.x -= 1
+		if Input.is_action_pressed("movement_right"):
+			dir.x += 1
+	else:
+		if Input.is_action_pressed("movement_left"):
+			rotate_y(SENSITIVITY)
+		if Input.is_action_pressed("movement_right"):
+			rotate_y(-SENSITIVITY)
+	return dir
 
 
 func _physics_process(delta):
-	var movement = get_input()
-	movement *= speed
+	var movement = transform.basis.xform(get_input()).normalized() * speed
 	if is_on_floor():
 		falling = 0
 	else:
-		falling = clamp(falling + delta * gravity, 0, 50)
+		falling = clamp(falling + delta * GRAVITY, 0, 50)
 	movement.y = -falling
+#warning-ignore:return_value_discarded
 	move_and_slide_with_snap(movement, Vector3.DOWN * 2, Vector3.UP)
+	if Input.is_action_pressed("look"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 
 
-func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		if event.relative.x > 0:
-			rotate_y(-lerp(0, sensitivity, event.relative.x/10))
-		elif event.relative.x < 0:
-			rotate_y(-lerp(0, sensitivity, event.relative.x/10))
+func _input(event):
+	if (event is InputEventMouseMotion
+		&& Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED):
+		rotate_y(-lerp(0, SENSITIVITY, event.relative.x/10))
