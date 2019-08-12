34feadddf34675a6e064b4d931c9f586b4e3173b
Brian Brunner <brian.brunner@mines.sdsmt.edu>
2019-08-11 20:56:17 -0500

    Add item registration

--- api/ItemRegistrar.gd
+++ api/ItemRegistrar.gd
@@ -1,8 +1,19 @@
 extends Node
 class_name myItemRegistrar
 
-var items := {}
+var __items := {}
 var Item = preload('res://abstract/Item.gd')
 
 func _ready():
 	pass
+
+
+func register(token: String, instance: myItem):
+	if token in __items:
+		Logger.error("Item %s has already been registered to %s, can't register to %s" % [token, __items[token], instance])
+	else:
+		__items[token] = instance
+
+
+func get(token: String):
+	return __items[token]
--- test/unit/api/test_ItemRegistrar.gd
+++ test/unit/api/test_ItemRegistrar.gd
@@ -5,7 +5,7 @@ var Item
 
 func before_all():
 	ItemRegistrar_t = preload('res://api/ItemRegistrar.gd')
-#	Item = preload('res://abstract/Item.gd')
+	Item = preload('res://abstract/Item.gd')
 #
 #
 func before_each():
@@ -15,7 +15,37 @@ func before_each():
 
 # Just to test project config.
 # The rest should use module scoped version
-func test_singleton_type2():
+func test_singleton_type():
 	assert_not_null(ItemRegistrar)
 	assert_is(get_node('/root/ItemRegistrar'), ItemRegistrar_t)
+
+
+func test_store_item():
+	var item = Item.new()
+	var token = 'items.test.testitem'
+	ItemRegistrar.register(token, item)
+	assert_eq(ItemRegistrar.get(token), item)
+
+
+func test_store_items():
+	var item1 = Item.new()
+	var token1 = 'items.test.testitem'
+	var item2 = Item.new()
+	var token2 = 'items.test.testitem2'
+	ItemRegistrar.register(token1, item1)
+	ItemRegistrar.register(token2, item2)
+	assert_eq(ItemRegistrar.get(token1), item1)
+	assert_eq(ItemRegistrar.get(token2), item2)
+
+
+func test_dupl_items():
+	var item1 = Item.new()
+	var token1 = 'items.test.testitem'
+	var item2 = Item.new()
+	ItemRegistrar.register(token1, item1)
+	ItemRegistrar.register(token1, item2)
+
+	# Don't override
+	# TODO check for log
+	assert_eq(ItemRegistrar.get(token1), item1)
 
